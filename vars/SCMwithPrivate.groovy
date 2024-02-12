de399b45bcf96a44cecf412de736de511cc9bab6
nishanth2429 <105205605+nishanth2429@users.noreply.github.com>
2024-02-12 14:06:31 +0530

    Update SCMwithPrivate.groovy

--- vars/SCMwithPrivate.groovy
+++ vars/SCMwithPrivate.groovy
@@ -3,7 +3,7 @@ def call(Map stageParams) {
     checkout([
         $class: 'GitSCM',
         branches: [[name:  stageParams.branch ]],
-        id: [[gitids: stageParams.gitid ]],
+        id: [[gitids: stageParams.credentialsId ]],
         userRemoteConfigs: [[ url: stageParams.url ]]
     ])
   }
