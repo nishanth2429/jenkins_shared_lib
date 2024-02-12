ba74ac210a984724c81193d531bd24fdb4a72099
nishanth2429 <105205605+nishanth2429@users.noreply.github.com>
2024-02-12 13:58:00 +0530

    Update gitCheckout.groovy

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,5 +1,8 @@
-// git branch: branch, credentialsId: credentialsId, url: url
-
-def call(credentialsId, url, branch){
-  git branch: branch, credentialsId: credentialsId, url: url
-}
+def call(Map stageParams) {
+ 
+    checkout([
+        $class: 'GitSCM',
+        branches: [[name:  stageParams.branch ]],
+        userRemoteConfigs: [[ url: stageParams.url ]]
+    ])
+  }
