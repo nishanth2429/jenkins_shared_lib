d25348ae6d551440539a17280e141061445f6d53
nishanth2429 <105205605+nishanth2429@users.noreply.github.com>
2024-02-16 12:13:04 +0530

    Update statiCodeAnalysis.groovy

--- vars/statiCodeAnalysis.groovy
+++ vars/statiCodeAnalysis.groovy
@@ -7,7 +7,6 @@ def call(credentialsId, String projectName, String projectKey){
     withSonarQubeEnv(credentialsId) {
         sh ''' 
         $SCANNER_HOME/bin/sonar-scanner -Dsonar.projectName=${projectName} -Dsonar.projectKey=${projectKey} -Dsonar.java.binaries=.
-        docker rmi ${ecr_repoName}:latest ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:lates        
         '''
     }
 }
