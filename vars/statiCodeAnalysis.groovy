5dcf43419545148565d021e8aec51de531047fcf
nishanth2429 <105205605+nishanth2429@users.noreply.github.com>
2024-02-16 13:31:14 +0530

    Update statiCodeAnalysis.groovy

--- vars/statiCodeAnalysis.groovy
+++ vars/statiCodeAnalysis.groovy
@@ -21,7 +21,7 @@
 def call(String Pname, String Pid) {
     withSonarQubeEnv('sonar-server') {
         sh """
-            ${tool 'SonarQubeScanner'}/bin/sonar-scanner \
+            ${tool 'SCANNER_HOME'}/bin/sonar-scanner \
             -Dsonar.projectName='${Pname}' \
             -Dsonar.projectKey='${Pid}' \
             -Dsonar.sources='.' \
