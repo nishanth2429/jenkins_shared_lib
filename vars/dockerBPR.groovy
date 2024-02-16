82ff44522c3465e72a3a2ff3f5b29c275fc1a30e
nishanth2429 <105205605+nishanth2429@users.noreply.github.com>
2024-02-16 14:03:47 +0530

    Update dockerBPR.groovy

--- vars/dockerBPR.groovy
+++ vars/dockerBPR.groovy
@@ -1,1 +1,11 @@
-
+def call(String project, String ImageTag, String hubUser){
+    
+    sh """
+     docker image build -t ${hubUser}/${project} . 
+     docker image tag ${hubUser}/${project}:latest
+     docker login -u nishanth00 -p Nishanth@00
+     docker image push ${hubUser}/${project}:latest
+     docker rmi ${hubUser}/${project}:latest
+     
+    """
+}
