faaf6d0f440a0efeb01ec43a3b70bebefbcbb398
nishanth2429 <105205605+nishanth2429@users.noreply.github.com>
2024-02-16 11:23:38 +0530

    Update statiCodeAnalysis.groovy

--- vars/statiCodeAnalysis.groovy
+++ vars/statiCodeAnalysis.groovy
@@ -1,6 +1,11 @@
-def call(credentialsId){
+def call(credentialsId,projectName,projectKey){
 
     withSonarQubeEnv(credentialsId: credentialsId) {
-         sh 'mvn clean package sonar:sonar'
+         // sh 'mvn clean package sonar:sonar'
+        // sh ''' $SCANNER_HOME/bin/sonar-scanner -Dsonar.projectName=project-1 -Dsonar.projectKey=project-1 -Dsonar.java.binaries=.  '''
+        sh ''' 
+        $SCANNER_HOME/bin/sonar-scanner -Dsonar.projectName=${projectName} -Dsonar.projectKey=projectKey -Dsonar.java.binaries=.  
+        
+        '''
     }
 }
