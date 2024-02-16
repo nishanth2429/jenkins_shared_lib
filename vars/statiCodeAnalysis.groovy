03e70b17e2364c78961323aab10549b41e18b863
nishanth2429 <105205605+nishanth2429@users.noreply.github.com>
2024-02-16 13:38:42 +0530

    Update statiCodeAnalysis.groovy

--- vars/statiCodeAnalysis.groovy
+++ vars/statiCodeAnalysis.groovy
@@ -1,26 +1,3 @@
-// def call(projectName, projectKey){
-// def call(){
-
-    // withSonarQubeEnv(credentialsId: credentialsId) {
-  
-         // sh 'mvn clean package sonar:sonar'
-        // sh ''' $SCANNER_HOME/bin/sonar-scanner -Dsonar.projectName=project-1 -Dsonar.projectKey=project-1 -Dsonar.java.binaries=.  '''
-    // withSonarQubeEnv('sonar-server') {
-        // sh ''' 
-        // $SCANNER_HOME/bin/sonar-scanner -Dsonar.projectName=${projectName} -Dsonar.projectKey=${projectKey} -Dsonar.java.binaries=.
-        // '''
-//         sh ''' $SCANNER_HOME/bin/sonar-scanner -Dsonar.projectName=project-1 -Dsonar.projectKey=project-1 -Dsonar.java.binaries=.  ''
-//     }
-// }
-
-// def call(String Pname, String Pid){
-//     withSonarQubeEnv('sonar-server') {
-//         sh ''' $SCANNER_HOME/bin/sonar-scanner -Dsonar.projectName=${Pname} -Dsonar.projectKey=${Pid} -Dsonar.java.binaries=.  '''
-//     }
-// }
-
- // -Dsonar.sources='.' \
- //            -Dsonar.java.binaries='./build/classes/java/main'
 def call(String Pname, String Pid) {
     withSonarQubeEnv('sonar-server') {
         sh """
@@ -31,4 +8,3 @@ def call(String Pname, String Pid) {
         """
     }
 }
-
