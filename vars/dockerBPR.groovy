1ccda7365faeeaf870b4df0231ae580959053efa
nishanth2429 <105205605+nishanth2429@users.noreply.github.com>
2024-02-16 15:05:50 +0530

    Update dockerBPR.groovy

--- vars/dockerBPR.groovy
+++ vars/dockerBPR.groovy
@@ -3,13 +3,14 @@ def call(String project, String hubUser){
     sh """
 
      docker image build -t '${hubUser}'/'${project}' . 
-     docker image tag '${hubUser}'/'${project}':latest
      docker login -u nishanth00 -p Nishanth@00
      docker image push '${hubUser}'/'${project}':latest
      docker rmi '${hubUser}'/'${project}':latest
      
     """
 }
+
+// docker image tag '${hubUser}'/'${project}':latest
 
      // sudo usermod -aG docker $USER
      // sudo chmod 666 /var/run/docker.sock
